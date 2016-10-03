10578d556ffa5a1939749981c1491cff2990b6b6
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2016-10-02 21:33:34 -0400

    Made tobat.bat more flexable.

--- tobat.bat
+++ tobat.bat
@@ -5,15 +5,12 @@
 ::ARGS
 ::
 ::tobat.bat - Part of the BatFramework
-::tobat.bat <input file> <output batch file>
+::tobat.bat <input file>
 ::
 ::        -input file : This is the file to specify that goes in. What happens
-:: to the file, is that it then turns into a batch file, and goes out as <outp
-::ut batch file>.
-::
-::        -output file (Optional) : This is the name of the output batch file.
-::Name it something like "output.bat" or something. If nothing is specified, i
-::t will just add a .bat extention to the input file's input.
+:: to the file, is that it then turns into a batch file, and goes out as the f
+::ile name with .bat. You can have as many input files as you want, it'll loop
+:: through all of the files.
 ::
 ::This BatFramework file turns any item (like example.exe) into a batch file, 
 ::and the batch file will re-create the executable or whatever file you speci
@@ -21,9 +18,12 @@
 ::reate example.exe
 ::
 
-set bframe_temp_outputfile=%~nx2
-if "%~nx1" == "" call throw.bat No argument specified for an input file.
-if "%~nx2" == "" set bframe_temp_outputfile=%~nx1.bat
+mkdir tmp
+
+:loopCreate
+
+if "%~nx1" == "" exit /b 0
+set bframe_temp_outputfile=%~nx1.bat
 set bframe_temp_outputfile=%bframe_temp_outputfile: =_%
 if NOT EXIST "%~nx1" call throw.bat The input file specified doesn't exist.
 
@@ -39,7 +39,7 @@ if "%bframe_silent%" == "true" ( echo.x>tmp\null.txt) else ( echo.Converting bas
 @echo.@echo.Creating ^"%~nx1^"...>>"%bframe_temp_outputfile%"
 @echo.@if EXIST ^"%~nx1^" del /f /a ^"%~nx1^">>"%bframe_temp_outputfile%"
 @echo.@if EXIST ^"%~nx1.b64^" del /f /a ^"%~nx1.b64^">>"%bframe_temp_outputfile%"
-@echo.@set x=^>^>^"%~nx1.b64^">>"%bframe_temp_outputfile%"
+@echo.@set x=^^^>^^^>^"%~nx1.b64^">>"%bframe_temp_outputfile%"
 
 for /f "tokens=* delims= " %%f in ( tmp/%bframe_temp_outputfile%.b64 ) do (
     
@@ -58,3 +58,7 @@ for /f "tokens=* delims= " %%f in ( tmp/%bframe_temp_outputfile%.b64 ) do (
 @echo.@echo.Conversion Successfull.>>"%bframe_temp_outputfile%"
 
 echo.Conversion complete.
+
+shift
+
+goto :loopCreate
